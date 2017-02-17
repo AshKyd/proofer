4a122efc2f00737b145ca8d6145cc30cbe81ec16
Ash Kyd <akyd@iix.net>
2017-02-17 21:38:19 +1000

    Integrate template support into command line app

--- bin/proofer.js
+++ bin/proofer.js
@@ -1,11 +1,56 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 const proofer = require('../src');
+const async = require('async');
+const program = require('commander');
+const copy = require('recursive-copy');
 
-const markdown = fs.readFileSync(process.argv[process.argv.length - 1], 'utf8');
+program
+  .version(require(path.join(__dirname, '../package.json')).version)
+  .option('-i, --apib [file]', 'API Blueprint input file')
+  .option('-o, --output [dir]', 'Output dir [stdout]', 'stdout')
+  .option('-t, --template [dir]', 'Also output HTML template in conjunction with -o [default]', 'none')
+  .parse(process.argv);
 
-proofer.parse(markdown, (error, result) => {
-  if (error) throw error;
-  console.log(JSON.stringify(result,null,2));
-})
+if (!program.apib) {
+  console.error('apib must be supplied with --apib');
+  process.exit();
+}
+
+async.waterfall([
+  // parse the apib
+  function parser(done) {
+    const markdown = fs.readFileSync(path.resolve(process.cwd(), program.apib), 'utf8');
+    proofer.parse(markdown, (error, result) => done(error, JSON.stringify(result)));
+  },
+
+  // Write to the output folder (or stdout)
+  function outputFolder(json, done) {
+    if (program.output === 'stdout') {
+      console.log(json);
+      process.exit();
+    }
+
+    // Load the specified template or the default template
+    let inputDir;
+    if (program.template === 'default') {
+      inputDir = path.join(__dirname, '../client');
+    } else if (program.template !== 'none') {
+      inputDir = path.resolve(process.cwd(), program.template);
+    }
+
+    const outputDir = path.resolve(process.cwd(), program.output);
+
+    // Make the output dir if it doesn't exist
+    if (!fs.statSync(outputDir)) fs.mkdirSync(outputDir);
+
+    // Write the JSON output
+    fs.writeFileSync(path.join(outputDir, 'output.json'), json);
+
+    if (inputDir) {
+      copy(inputDir, outputDir, done);
+    }
+  },
+]);
--- client/init.js
+++ client/init.js
@@ -1,57 +1,61 @@
-const fs = require('fs');
-const path = require('path');
-const data = JSON.parse(fs.readFileSync(path.join(__dirname,'./data.json'), 'utf8'));
-const ractiveTemplate = fs.readFileSync(path.join(__dirname, './template.ractive'), 'utf8');
 const MarkdownIt = require('markdown-it');
-const md = new MarkdownIt({ html: true });
-const parseHash = hash => {
-  let index = hash.indexOf('+') - 1;
-  if(index < 0) index = undefined;
-  return hash.substr(1, index);
-};
-const slugify = data.slugify = string => String(string).toLowerCase().replace(/\s/g,'-').replace(/[^a-z0-9-]/g, '');
-const markdown = data.markdown = string => md.render(string).replace(/<table>/g, '<table class="table">');
-const isHeroSection = data.isHeroSection = content => {
-  const isHero = content.element === 'copy' && content.content.length < 500;
-  return isHero;
-};
+function init(data, ractiveTemplate){
+  const md = new MarkdownIt({ html: true });
+  const parseHash = hash => {
+    let index = hash.indexOf('+') - 1;
+    if(index < 0) index = undefined;
+    return hash.substr(1, index);
+  };
+  const slugify = data.slugify = string => String(string).toLowerCase().replace(/\s/g,'-').replace(/[^a-z0-9-]/g, '');
+  const markdown = data.markdown = string => md.render(string).replace(/<table>/g, '<table class="table">');
+  const isHeroSection = data.isHeroSection = content => {
+    const isHero = content.element === 'copy' && content.content.length < 500;
+    return isHero;
+  };
 
-data.windowHash = parseHash(window.location.hash);
-data.categories.forEach(cat => cat.categoryHash = slugify(cat.categoryTitle));
-data.showId = {};
+  data.windowHash = parseHash(window.location.hash);
+  data.categories.forEach(cat => cat.categoryHash = slugify(cat.categoryTitle));
+  data.showId = {};
 
-var ractive = new Ractive({
-  el: '#container',
-  template: ractiveTemplate,
-  data: data,
-});
+  var ractive = new Ractive({
+    el: '#container',
+    template: ractiveTemplate,
+    data: data,
+  });
 
-let editor;
-const hashchange = () => {
-  if(editor) editor.destroy();
-  const windowHash = parseHash(window.location.hash);
-  ractive.set('windowHash', windowHash);
-  if(windowHash.includes('ace-editor')){
-    scrollTo(0,0);
-    const keyPath = windowHash.replace(/^ace-editor-/, '');
-    const content = ractive.get(keyPath)
+  let editor;
+  const hashchange = () => {
+    if(editor) editor.destroy();
+    const windowHash = parseHash(window.location.hash);
+    ractive.set('windowHash', windowHash);
+    if(windowHash.includes('ace-editor')){
+      scrollTo(0,0);
+      const keyPath = windowHash.replace(/^ace-editor-/, '');
+      const content = ractive.get(keyPath)
 
-    editor = ace.edit("editor");
-    editor.setOptions({
-      maxLines: Infinity
-    });
-    editor.setTheme("ace/theme/chrome");
-    editor.getSession().setMode("ace/mode/javascript");
-    document.getElementById('editor').style.fontSize='1em';
-    editor.setValue(content.content, 1);
-  }
-};
-window.addEventListener("hashchange", hashchange, false);
-hashchange();
+      editor = ace.edit("editor");
+      editor.setOptions({
+        maxLines: Infinity
+      });
+      editor.setTheme("ace/theme/chrome");
+      editor.getSession().setMode("ace/mode/javascript");
+      document.getElementById('editor').style.fontSize='1em';
+      editor.setValue(content.content, 1);
+    }
+  };
+  window.addEventListener("hashchange", hashchange, false);
+  hashchange();
 
-ractive.once('init', () => {
-  if(window.location.hash === '#') return;
-  document.querySelector(window.location.hash).scrollIntoView();
-})
+  ractive.once('init', () => {
+    if(window.location.hash === '#') return;
+    document.querySelector(window.location.hash).scrollIntoView();
+  })
 
-ractive.on('categorychange', () => scrollTo(0,0));
+  ractive.on('categorychange', () => scrollTo(0,0));
+}
+
+Promise.all([
+    fetch('data.json').then(response => response.json()),
+    fetch('template.ractive').then(response => response.text()),
+  ])
+  .then((responses) => init(responses[0], responses[1]));
